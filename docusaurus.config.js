f05b11f09172e5039823c3515ad44a4d91328895
fattie0926 <fattie@fattie.io>
2025-07-20 17:25:05 +0800

    Sync from Obsidian at 2025-07-20 17:25:05

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://jaron.tw',
+  url: 'https://fattie0926.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
